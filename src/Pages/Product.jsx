c5db4092c52323cd75c5cbd4a6ab4194cb1696b7
tingtong <kkoemtong@gmail.com>
2025-11-21 11:44:45 +0700

    for_back

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -51,6 +51,7 @@ const Product = () => {
             />
           </div>
         </div>
+        <div></div>
 
         {/* Filter Section */}
         <div className="mb-8 pb-6 border-b-4 border-black">
